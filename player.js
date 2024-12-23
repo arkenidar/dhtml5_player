930ee2e456aed688839eb55944daffc13167c92c
Dario Cangialosi <dario.cangialosi@gmail.com>
2024-12-23 13:56:28 +0100

    Enhance playback logic to allow custom first link selection based on URL hash

--- player.js
+++ player.js
@@ -14,7 +14,6 @@ const linksFilterCriterion = itemToFilter => anyArgumentsSome(
 document.addEventListener("DOMContentLoaded", () => {
     var links = document.querySelectorAll("a[href]")
     links = Array.from(links).filter(linksFilterCriterion)
-    first_link = links[0]
     for (var link_index in links) {
         link_index = parseInt(link_index)
         var link = links[link_index]
@@ -36,10 +35,36 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     if (links.length >= 1) {
         player_feature.style.display = "block"
-        play(links[0], true) //player.stop()
+        first_link = links[0]
+        // custom first link in location.hash
+        if (location.hash.startsWith("#")) {
+            var hash = location.hash.slice(1)
+            hash = decodeURIComponent(hash)
+
+            // default keyword to filter by
+            var first_link_override = undefined
+            if (hash.startsWith("JSON:")) {
+                hash = hash.slice(5)
+                // if hash is a JSON string
+                try {
+                    // parse JSON string
+                    var hash_json = JSON.parse(hash)
+                    // if JSON string has keyword property
+                    if (typeof hash_json.keyword != "undefined") {
+                        first_link_override = links.filter(link => link.href.includes(hash_json.keyword))[0]
+                    }
+                } catch (error) {
+                    console.error(hash, error)
+                }
+            }
+            if (typeof first_link_override != "undefined") {
+                first_link = first_link_override
+            }
+        }
+        play(first_link, false)
     }
 })
-function play(link, stop = false) {
+function play(link, player_play = true) {
     if (typeof link == "undefined") return
     if (typeof current_link != "undefined")
         current_link.classList.remove("current_link")
@@ -47,7 +72,7 @@ function play(link, stop = false) {
     current_link.classList.add("current_link")
     playing.innerText = link.innerText
     player.src = link.href
-    if (!stop) player.play()
+    if (player_play) player.play()
 }
 player.onended =
     event =>
